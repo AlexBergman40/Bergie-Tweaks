3ad8822339f4d6e689bea7f5ade252044fa7bcd5
Alex Bergman <alexbergman40@gmail.com>
2026-02-08 22:00:29 -0800

    Initial Commit

---
+++ src/client/java/org/bergie/oldschoolEnchanting/client/OldschoolEnchantingClient.java
@@ -0,0 +1,10 @@
+package org.bergie.oldschoolEnchanting.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OldschoolEnchantingClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/bergie/oldschoolEnchanting/OldschoolEnchanting.java
@@ -0,0 +1,10 @@
+package org.bergie.oldschoolEnchanting;
+
+import net.fabricmc.api.ModInitializer;
+
+public class OldschoolEnchanting implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
